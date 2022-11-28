1e523ab379dda1c43ecc9e9e1a5b81af389be0bb
ebin <ebinsu@chancetop.com>
2022-11-28 16:44:24 +0800

    -

---
+++ 01-providers.tf
@@ -0,0 +1,46 @@
+terraform {
+  required_version = ">= 0.13"
+
+  required_providers {
+    github = {
+      source  = "integrations/github"
+      version = ">= 4.5.2"
+    }
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = ">= 2.0.2"
+    }
+    kubectl = {
+      source  = "gavinbunney/kubectl"
+      version = ">= 1.10.0"
+    }
+    flux = {
+      source  = "fluxcd/flux"
+      version = ">= 0.0.13"
+    }
+    tls = {
+      source  = "hashicorp/tls"
+      version = "3.1.0"
+    }
+    sops = {
+      source  = "carlpett/sops"
+      version = ">= 0.7.0"
+    }
+  }
+}
+
+provider "sops" {}
+
+provider "flux" {}
+
+provider "kubectl" {}
+
+provider "kubernetes" {
+  config_path = "/k3s.yaml"
+}
+
+provider "github" {
+  # Configuration options
+  owner = var.github_owner
+  token = data.sops_file.secrets.data["github_token"]
+}
---
+++ 02-variables.tf
@@ -0,0 +1,33 @@
+variable "github_owner" {
+  type        = string
+  description = "github owner"
+  default     = "fvxiaoyi"
+}
+
+variable "repository_name" {
+  type        = string
+  default     = "tencent-cloud-flux-cd"
+  description = "github repository name"
+}
+
+variable "repository_visibility" {
+  type        = string
+  default     = "public"
+  description = "How visible is the github repo"
+}
+
+variable "branch" {
+  type        = string
+  default     = "main"
+  description = "branch name"
+}
+
+variable "target_path" {
+  type        = string
+  default     = "cluster"
+  description = "flux sync target path"
+}
+
+data "sops_file" "secrets" {
+  source_file = "../secrets/secrets.enc.json"
+}
